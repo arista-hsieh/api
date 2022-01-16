326fc783866d9fc7f3c78288d8d0b6ad9fb9f655
Arista <89002597+arista-hsieh@users.noreply.github.com>
2022-01-16 22:26:32 +0800

    Add files via upload

---
+++ products.js
@@ -0,0 +1,44 @@
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.esm-browser.js';
+
+createApp({
+    data(){
+        return{
+            apiUrl: 'https://vue3-course-api.hexschool.io/v2',
+            apiPath: 'arista',
+            products: [],
+            temp: {},
+        }
+    },
+    methods:{
+        checkAdmin(){
+            const url = `${this.apiUrl}/api/user/check`
+            axios.post(url)
+            .then(()=>{
+                this.getData();
+            })
+            .catch((err)=>{
+                alert(err.data.message)
+                window.location = 'index.html'
+            })
+        },
+        getData(){
+            const url = `${this.apiUrl}/api/${this.apiPath}/admin/products`;
+            axios.get(url)
+            .then((response)=>{
+                this.products = response.data.products;
+            })
+            .catch((err)=>{
+                alert(err.data.mesage);
+            })
+        },
+        openProduct(item){
+            this.tempProduct = item;
+        }
+    },
+    mounted(){
+        const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
+        axios.defaults.headers.common.Authorization = token;
+
+        this.checkAdmin()
+    }
+}).mount('#app');
